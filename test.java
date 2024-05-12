64e4e1cc4b5545401d26052ea7743e7d7b00f808
Jiel109 <168947664+Jiel109@users.noreply.github.com>
2024-05-13 01:07:58 +0800

    test 2 with simple recursion
    added a recursion where it will return to the start, but the balance will not update

--- test.java
+++ test.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        balance();
+    }
+    public static void balance()
+    {
         Scanner input = new Scanner(System.in);
         ArrayList<String> history = new ArrayList<>();
         
@@ -12,17 +17,17 @@ public class test {
 
         System.out.println(currency + initial_balance);
 
-        String cash_in = "Cash In 1"; 
-        String cash_out = "Cash Out 2";
+        String cash_in = "Enter 1 for Cash In"; 
+        String cash_out = "Enter 2 for Cash Out";
+        String exit = "Enter 3 for Exit";
 
-        System.out.print(cash_in + " --- " +cash_out);
-        System.out.print( '\n'+"Choose:");
+        System.out.print(cash_in + "\n--------------------\n" +cash_out + "\n--------------------\n" + exit + "\n--------------------\n");
         int in_out = input.nextInt();
 
         switch (in_out) {
             case 1:
                 
-                System.out.print("How much to Cash in:");
+                System.out.print("How much to Cash in: ");
                 int in_transaction = input.nextInt() ;
 
                 initial_balance = initial_balance + in_transaction;
@@ -32,37 +37,44 @@ public class test {
                 System.out.println(history_in);
 
                 history.add(history_in);
-
+                System.out.println("Transaction History:");
+        
+                for (String str : history) {
+                    System.out.println(str);
+                }
+    
+                System.out.println("New balance: " + initial_balance);
+                balance();
                 break;
         
 
             case 2:
-                System.out.print("How much to Cash:");
+                System.out.print("How much to Cash out: ");
                 int out_transaction = input.nextInt() ;
 
                 initial_balance = initial_balance - out_transaction;
 
-                String history_out = "Cash in of $" + out_transaction;
+                String history_out = "Cash out of $" + out_transaction;
 
                 System.out.println(history_out);
 
                 history.add(history_out);
-
+                System.out.println("Transaction History:");
+        
+                for (String str : history) {
+                    System.out.println(str);
+                }
+    
+                System.out.println("New balance: " + initial_balance);
+                balance();
                 break;
-                
+            case 3:
+                System.out.println("Thank you for using the program!");
+                break;
 
 
             default:
                 break;
         }
-
-        System.out.println("Transaction History:");
-        
-        for (String str : history) {
-            System.out.println(str);
-        }
-    
-        System.out.println(initial_balance);
-
     }
 }
